9cd405740d0f8446727455330f4765c51b05d49e
Darío Rivera <fermius.us@gmail.com>
2022-08-24 17:13:00 -0500

    refactor: group conditions for months

--- datetime/date.c
+++ datetime/date.c
@@ -15,28 +15,19 @@ int days_per_month(int month)
     switch (month)
     {
         case JANUARY:
-            return 31;
-        case FEBRUARY:
-            return is_leap_year(month) ? 28 : 29;
         case MARCH:
-            return 31;
-        case APRIL:
-            return 30;
         case MAY:
-            return 31;
-        case JUNE:
-            return 30;
         case JULY:
-            return 31;
         case AUGUST:
-            return 31;
-        case SEPTEMBER:
-            return 30;
         case OCTOBER:
-            return 31;
-        case NOVEMBER:
-            return 30;
         case DECEMBER:
             return 31;
+        case APRIL:
+        case JUNE:
+        case SEPTEMBER:
+        case NOVEMBER:
+            return 30;
+        case FEBRUARY:
+            return is_leap_year(month) ? 28 : 29;
     }
 }
