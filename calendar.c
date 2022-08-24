0310263705ab4a5bf56888e85191f5155ad2a47b
Darío Rivera <fermius.us@gmail.com>
2022-08-24 18:04:04 -0500

    refactor: get current month calendar

--- calendar.c
+++ calendar.c
@@ -1,57 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "utils/string_utils.h"
 #include "datetime/date.h"
 
 void print_month_name(int month);
+void print_month_calendar(int year, int month);
+
+const int FIRST_AD_DAY = 6; // saturday
 
 int main()
 {
-    const int FIRST_AD_DAY = 6; // saturday
+    int month;
 
-    int january_first_day, _days_per_month, month, date, year;
+    time_t current_time;
+    current_time = time(NULL);
+    struct tm c_localtime = *localtime(&current_time);
 
-    printf("Type a year: ");
-    scanf("%d", &year);
+    print_month_calendar(c_localtime.tm_year + 1900, c_localtime.tm_mon + 1);
 
-    january_first_day = FIRST_AD_DAY;
+    return 0;
+}
+
+void print_month_calendar(int year, int month)
+{
+    print_month_name(month);
+
+    printf("\nMon  Tue  Wed  Thr  Fri  Sat  Sun\n");
+    printf("---  ---  ---  ---  ---  ---  ---\n");
+
+    int january_first_day = FIRST_AD_DAY;
 
     // first day for the selected year
     for (int k = 1; k < year; k++) {
         january_first_day = (january_first_day + days_for_year(k) ) % 7;
     }
 
-    for (month = JANUARY; month <= DECEMBER; month++)
-    {
-        print_month_name(month);
-        _days_per_month = days_per_month(month);
-
-        printf("\nMon  Tue  Wed  Thr  Fri  Sat  Sun\n");
-        printf("---  ---  ---  ---  ---  ---  ---\n");
-
-        for (date = 1; date <= 5 * january_first_day - 5; date++) {
-            printf(" ");
-        }
-
-        for (date = 1; date <= _days_per_month; date++) {
-            int day_block_lenght = 3;
-            char day_text[3];
-            sprintf(day_text, "%d", date);
-            str_pad(day_text, day_block_lenght, " ", STR_PAD_LEFT);
-            printf("%s", day_text);
-
-            if ( (date + january_first_day - 1) % 7 > 0 )
-                printf("%2s", " ");
-            else
-                printf("\n");
-        }
-
-        january_first_day = (january_first_day + _days_per_month) % 7;
+    for (int date = 1; date <= 5 * january_first_day - 5; date++) {
+        printf(" ");
     }
 
-    printf("\n");
+    int _days_per_month = days_per_month(month);
 
-    return 0;
+    for (int date = 1; date <= _days_per_month; date++) {
+        int day_block_lenght = 3;
+        char day_text[3];
+        sprintf(day_text, "%d", date);
+        str_pad(day_text, day_block_lenght, " ", STR_PAD_LEFT);
+        printf("%s", day_text);
+
+        if ( (date + january_first_day - 1) % 7 > 0 )
+            printf("%2s", " ");
+        else
+            printf("\n");
+    }
+
+    january_first_day = (january_first_day + _days_per_month) % 7;
+
+    printf("\n");
 }
 
 void print_month_name(int month)
@@ -62,37 +68,37 @@ void print_month_name(int month)
             printf("\nJanuary\n");
             break;
         case FEBRUARY:
-            printf("\n\n\nFebruary\n");
+            printf("\nFebruary\n");
             break;
         case MARCH:
-            printf("\n\n\nMarch\n");
+            printf("\nMarch\n");
             break;
         case APRIL:
-            printf("\n\n\nApril\n");
+            printf("\nApril\n");
             break;
         case MAY:
-            printf("\n\n\nMay\n");
+            printf("\nMay\n");
             break;
         case JUNE:
-            printf("\n\n\nJune\n");
+            printf("\nJune\n");
             break;
         case JULY:
-            printf("\n\n\nJuly\n");
+            printf("\nJuly\n");
             break;
         case AUGUST:
-            printf("\n\n\nAugust\n");
+            printf("\nAugust\n");
             break;
         case SEPTEMBER:
-            printf("\n\n\nSeptember\n");
+            printf("\nSeptember\n");
             break;
         case OCTOBER:
-            printf("\n\n\nOctober\n");
+            printf("\nOctober\n");
             break;
         case NOVEMBER:
-            printf("\n\n\nNovember\n");
+            printf("\nNovember\n");
             break;
         case DECEMBER:
-            printf("\n\n\nDecember\n");
+            printf("\nDecember\n");
             break;
     }
 }
